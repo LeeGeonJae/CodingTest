add9040115dcba3a8e5a8a1288cdc7aff4587b11
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-02-18 21:26:34 +0900

    [코딩테스트] 프로그래머스 Lv2 : 행렬의 곱셈

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,9 +1,28 @@
+#include <string>
 #include <vector>
-#include <unordered_set>
+
 using namespace std;
 
-int solution(vector<int> nums)
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 {
-	unordered_set<int> poket(nums.begin(), nums.end());
-	return min(nums.size() / 2, poket.size());
+    vector<vector<int>> answer;
+    int col = arr1.size();
+    int row = arr2[0].size();
+    int size = arr2.size();
+    for (int i = 0; i < col; i++)
+    {
+        vector<int> number(row);
+        for (int j = 0; j < row; j++)
+        {
+            int num = 0;
+            for (int k = 0; k < size; k++)
+                num += arr1[i][k] * arr2[k][j];
+
+            number[j] = num;
+        }
+        answer.push_back(number);
+    }
+
+
+    return answer;
 }
