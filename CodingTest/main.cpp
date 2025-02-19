c8a6418bb08b6f4bb447f08fbd9a4f9fca564dfd
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-02-19 18:36:21 +0900

    [코딩테스트] 프로그래머스 lv2 : 1차 캐시 (2018 카카오)

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,18 +1,59 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-const int WeekNumber = 7;
-const int MonthNumber = 12;
-
-string Week[WeekNumber] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
-int Days[MonthNumber] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-string solution(int a, int b)
+bool checkCityName(const string& lhs, const string& rhs)
 {
-    int number = 4 + b;
-    for (int i = 0; i < a - 1; i++)
-        number += Days[i];
-    return Week[number % WeekNumber];
+    if (lhs.size() != rhs.size())
+        return false;
+
+    for (int i = 0; i < lhs.size(); i++)
+    {
+        if (!(lhs[i] == rhs[i]
+            || lhs[i] - 'A' + 'a' == rhs[i]
+            || rhs[i] - 'A' + 'a' == lhs[i]))
+            return false;
+    }
+
+    return true;
+}
+
+int solution(int cacheSize, vector<string> cities)
+{
+    vector<int> cacheLife(cacheSize, 100000);
+    vector<string> cache(cacheSize);
+    int currentTime = cities.size();
+
+    for (string& city : cities)
+    {
+        bool isCheck = false;
+
+        // 비교
+        for (int i = 0; i < cache.size(); i++)
+        {
+            if (cacheSize && checkCityName(city, cache[i]))
+            {
+                cacheLife[i] = -1;
+                isCheck = true;
+                break;
+            }
+        }
+
+        // 캐시 갱신
+        if (!isCheck && !cacheLife.empty())
+        {
+            int index = max_element(cacheLife.begin(), cacheLife.end()) - cacheLife.begin();
+            cache[index] = city;
+            cacheLife[index] = -1;
+        }
+
+        for (int i = 0; i < cacheLife.size(); i++)
+            cacheLife[i]++;
+
+        currentTime += isCheck ? 0 : 4;
+    }
+
+    return currentTime;
 }
