d5fc80d32fee8b5ead81b59cdfbb649e2e2d1d9a
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-02-28 01:56:07 +0900

    [코딩테스트] 프로그래머스 lv2 : 전화번호 목록 - 해시 함수 사용하여 풀기

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,24 +1,23 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-bool solution(vector<string> phone_book)
+bool solution(vector<string> phone_book) 
 {
-    sort(phone_book.begin(), phone_book.end(), [](string& lhs, string& rhs)
+    unordered_map<string, int> hash_map;
+    for (int i = 0; i < phone_book.size(); i++)
+        hash_map[phone_book[i]] = 1;
+
+    for (int i = 0; i < phone_book.size(); i++) {
+        string phone_number = "";
+        for (int j = 0; j < phone_book[i].size(); j++) 
         {
-            return lhs < rhs;
-        });
-
-    for (int i = 0; i < phone_book.size() - 1; i++)
-    {
-        if (phone_book[i][0] != phone_book[i + 1][0])
-            continue;
-
-        string temp = phone_book[i + 1].substr(0, phone_book[i].size());
-        if (phone_book[i] == temp)
-            return false;
+            phone_number += phone_book[i][j];
+            if (hash_map[phone_number] && phone_number != phone_book[i])
+                return false;
+        }
     }
 
     return true;
