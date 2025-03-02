1928a18fefb4d5e91e21754f1b77314c1b46aa14
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-02 16:23:41 +0900

    [코딩테스트] 프로그래머스 lv2 : [1차] 뉴스 클러스터링

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,21 +1,56 @@
 #include <string>
-#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-int solution(int n)
+char check(char& word)
 {
-    vector<int> board(n + 1);
-    vector<int> prime;
-    for (int i = 2; i <= n; i++)
+    if (word >= 'A' && word <= 'Z')
+        return word;
+    else if (word >= 'a' && word <= 'z')
+        return word + 'A' - 'a';
+    return 0;
+}
+
+int solution(string str1, string str2)
+{
+    int answer = 0;
+    int n = 0;
+    unordered_map<string, int> board;
+
+    for (int i = 0; i < str1.size() - 1; i++)
     {
-        if (!board[i])
+        char lhs = check(str1[i]);
+        char rhs = check(str1[i + 1]);
+
+        if (lhs && rhs)
         {
-            prime.push_back(i);
-            for (int j = i; j <= n; j = j + i)
-                board[j]++;
+            string temp;
+            temp.append({ lhs, rhs });
+            board[temp]++;
+            n++;
+        }
+    }
+    for (int i = 0; i < str2.size() - 1; i++)
+    {
+        char lhs = check(str2[i]);
+        char rhs = check(str2[i + 1]);
+
+        if (lhs && rhs)
+        {
+            string temp;
+            temp.append({ lhs, rhs });
+
+            if (board[temp] > 0)
+            {
+                answer++;
+                n--;
+            }
+
+            board[temp]--;
+            n++;
         }
     }
 
-    return prime.size();
+    return board.empty() ? 65536 : answer * 65536 / n;
 }
