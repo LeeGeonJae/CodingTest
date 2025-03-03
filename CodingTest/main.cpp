0d7025983c410742f3253fdbf647dba6ff582a81
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-04 03:30:37 +0900

    [코딩테스트] 프로그래머스 lv3 : 정수 삼각형 - Bottom-up 동적 계획법

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,38 +1,14 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> solution(int N, vector<int> stages)
+int solution(vector<vector<int>> triangle)
 {
-    unordered_map<int, int> stageMap;
+    for (int i = triangle.size() - 1; i > 0; i--)
+        for (int j = 0; j < triangle[i].size() - 1; j++)
+            triangle[i - 1][j] += max(triangle[i][j], triangle[i][j + 1]);
 
-    for (int i = 0; i < stages.size(); i++)
-    {
-        if (stages[i] <= N)
-            stageMap[stages[i]]++;
-    }
-
-    int num = stages.size();
-    vector<pair<int, double>> rate;
-    for (int i = 1; i <= N; i++)
-    {
-        rate.push_back(make_pair(i, stageMap[i] ? (num ? (double)stageMap[i] / num : 1) : 0));
-        num -= stageMap[i];
-    }
-
-    sort(rate.begin(), rate.end(), [](auto& lhs, auto& rhs)
-        {
-            if (lhs.second == rhs.second)
-                return lhs.first < rhs.first;
-            return lhs.second > rhs.second;
-        });
-
-    vector<int> answer;
-    for (auto& r : rate)
-        answer.push_back(r.first);
-
-    return answer;
+    return triangle[0][0];
 }
