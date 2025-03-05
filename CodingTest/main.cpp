3d8c356d05a58f591833c40b02e8b72f889683b1
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-05 21:37:50 +0900

    [코딩테스트] 프로그래머스 lv3 : 야근 지수 - 우선 순위 큐

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,27 +1,28 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
 long long solution(int n, vector<int> works)
 {
-    sort(works.rbegin(), works.rend());
-    int index = 0;
+    priority_queue<int> pq(works.begin(), works.end());
     for (int i = 0; i < n; i++)
     {
-        if (works[index] > 0)
-            works[index]--;
-
-        if (index + 1 != works.size())
-            index += works[index] < works[index + 1] ? 1 : 0;
-
-        index = works[index] < works[0] ? 0 : index;
+        int n = pq.top();
+        if (n != 0)
+        {
+            pq.pop();
+            pq.push(--n);
+        }
     }
 
     long long answer = 0;
-    for (int i = 0; i < works.size(); i++)
-        answer += works[i] * works[i];
+    while (!pq.empty())
+    {
+        answer += pq.top() * pq.top();
+        pq.pop();
+    }
 
     return answer;
 }
