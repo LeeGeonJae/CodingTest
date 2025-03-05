bac5dafe6fc5e67f9ebb211abbc49f9f61073505
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-05 21:22:54 +0900

    [코딩테스트] 프로그래머스 lv3 : 야근지수

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,28 +1,27 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-string solution(int n, int t, int m, int p)
+long long solution(int n, vector<int> works)
 {
-    char code[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-
-    string number = "0";
-    for (int i = 1; i < m * t; i++)
+    sort(works.rbegin(), works.rend());
+    int index = 0;
+    for (int i = 0; i < n; i++)
     {
-        int num = i;
-        string temp;
-        while (num > 0)
-        {
-            temp = code[num % n] + temp;
-            num = num / n;
-        }
-        number += temp;
+        if (works[index] > 0)
+            works[index]--;
+
+        if (index + 1 != works.size())
+            index += works[index] < works[index + 1] ? 1 : 0;
+
+        index = works[index] < works[0] ? 0 : index;
     }
 
-    string answer;
-    for (int i = 0; i < t; i++)
-        answer += number[i * m + p - 1];
+    long long answer = 0;
+    for (int i = 0; i < works.size(); i++)
+        answer += works[i] * works[i];
 
     return answer;
 }
