d8824d974371c6d3d7e90f7da1901dd2f119afb5
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-06 23:43:09 +0900

    [코딩테스트] 프로그래머스 lv2 : 주식가격 (스택/큐) - 스택

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,20 +1,21 @@
+#include <string>
 #include <vector>
 #include <stack>
 
 using namespace std;
 
-vector<int> solution(vector<int> numbers)
+vector<int> solution(vector<int> prices)
 {
-    vector<int> answer(numbers.size(), -1);
+    vector<int> answer(prices.size());
     stack<int> st;
-    for (int i = 0; i < numbers.size(); i++)
+    for (int i = 0; i < prices.size(); i++)
     {
-        while (!st.empty() && numbers[st.top()] < numbers[i])
+        answer[i] = prices.size() - 1 - i;
+        while (!st.empty() && prices[st.top()] > prices[i])
         {
-            answer[st.top()] = numbers[i];
+            answer[st.top()] = i - st.top();
             st.pop();
         }
-
         st.push(i);
     }
 
