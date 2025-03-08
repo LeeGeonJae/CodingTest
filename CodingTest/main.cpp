80001ed4f1b9843757ef7f71a1758d716dce2f47
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-09 03:35:28 +0900

    [코딩테스트] 프로그래머스 lv3 : 단어변환 - DFS

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,17 +1,72 @@
-#include <iostream>
+#include <string>
 #include <vector>
-#include <algorithm>
-#include <set>
+#include <stack>
+#include <unordered_map>
+#include <math.h>
+
 using namespace std;
 
-int solution(vector<vector<int> > land)
+void DFS(stack<string>& q, unordered_map<string, vector<string>>& wordMap
+    , unordered_map<string, bool>& checkMap, int& number, string& target)
 {
-    for (int i = 1; i < land.size(); i++)
+    if (q.top() == target)
     {
-        land[i][0] += max(land[i - 1][1], max(land[i - 1][2], land[i - 1][3]));
-        land[i][1] += max(land[i - 1][0], max(land[i - 1][2], land[i - 1][3]));
-        land[i][2] += max(land[i - 1][0], max(land[i - 1][1], land[i - 1][3]));
-        land[i][3] += max(land[i - 1][0], max(land[i - 1][1], land[i - 1][2]));
+        number = number ? (number > q.size() - 1 ? q.size() - 1 : number) : q.size() - 1;
+        return;
     }
-    return *max_element(land[land.size() - 1].begin(), land[land.size() - 1].end());
+
+    for (int i = 0; i < wordMap[q.top()].size(); i++)
+    {
+        if (!checkMap[wordMap[q.top()][i]] && (number == 0 ? 1 : number > q.size()))
+        {
+            q.push(wordMap[q.top()][i]);
+            checkMap[q.top()] = true;
+            DFS(q, wordMap, checkMap, number, target);
+            checkMap[q.top()] = false;
+            q.pop();
+        }
+    }
+}
+
+int solution(string begin, string target, vector<string> words)
+{
+    unordered_map<string, vector<string>> wordMap;
+    unordered_map<string, bool> checkMap;
+    checkMap[begin] = false;
+    for (auto& s : words)
+    {
+        int num = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            num += (begin[i] != s[i]);
+            if (num > 1)
+                break;
+        }
+        if (num == 1)
+            wordMap[begin].push_back(s);
+    }
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        checkMap[words[i]] = false;
+        for (int j = 0; j < words.size(); j++)
+        {
+            int num = 0;
+            for (int k = 0; k < words[i].size(); k++)
+            {
+                num += (words[i][k] != words[j][k]);
+                if (num > 1)
+                    break;
+            }
+
+            if (num == 1)
+                wordMap[words[i]].push_back(words[j]);
+        }
+    }
+
+    int answer = 0;
+    stack<string> q;
+    q.push(begin);
+    DFS(q, wordMap, checkMap, answer, target);
+    return answer;
 }
