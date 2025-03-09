5fd0633d0bbd47f86076a43448ae21d963ae670e
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-10 02:43:49 +0900

    [코딩테스트] 프로그래머스 lv2 : 스킬트리

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,72 +1,30 @@
 #include <string>
 #include <vector>
-#include <stack>
-#include <unordered_map>
-#include <math.h>
 
 using namespace std;
 
-void DFS(stack<string>& q, unordered_map<string, vector<string>>& wordMap
-    , unordered_map<string, bool>& checkMap, int& number, string& target)
+int solution(string skill, vector<string> skill_trees)
 {
-    if (q.top() == target)
-    {
-        number = number ? (number > q.size() - 1 ? q.size() - 1 : number) : q.size() - 1;
-        return;
-    }
-
-    for (int i = 0; i < wordMap[q.top()].size(); i++)
-    {
-        if (!checkMap[wordMap[q.top()][i]] && (number == 0 ? 1 : number > q.size()))
-        {
-            q.push(wordMap[q.top()][i]);
-            checkMap[q.top()] = true;
-            DFS(q, wordMap, checkMap, number, target);
-            checkMap[q.top()] = false;
-            q.pop();
-        }
-    }
-}
-
-int solution(string begin, string target, vector<string> words)
-{
-    unordered_map<string, vector<string>> wordMap;
-    unordered_map<string, bool> checkMap;
-    checkMap[begin] = false;
-    for (auto& s : words)
-    {
-        int num = 0;
-        for (int i = 0; i < s.size(); i++)
-        {
-            num += (begin[i] != s[i]);
-            if (num > 1)
-                break;
-        }
-        if (num == 1)
-            wordMap[begin].push_back(s);
-    }
-
-    for (int i = 0; i < words.size(); i++)
-    {
-        checkMap[words[i]] = false;
-        for (int j = 0; j < words.size(); j++)
-        {
-            int num = 0;
-            for (int k = 0; k < words[i].size(); k++)
-            {
-                num += (words[i][k] != words[j][k]);
-                if (num > 1)
-                    break;
-            }
-
-            if (num == 1)
-                wordMap[words[i]].push_back(words[j]);
-        }
-    }
+    vector<int> skillCheck(26);
+    for (int i = 0; i < skill.size(); i++)
+        skillCheck[skill[i] - 'A'] = 1;
 
     int answer = 0;
-    stack<string> q;
-    q.push(begin);
-    DFS(q, wordMap, checkMap, answer, target);
+    for (string& s : skill_trees)
+    {
+        int index = 0;
+        for (int j = 0; j < s.size(); j++)
+        {
+            if (skillCheck[s[j] - 'A'] && skill[index] != s[j])
+            {
+                answer--;
+                break;
+            }
+            else if (skillCheck[s[j] - 'A'])
+                index += index + 1 == skill.size() ? 0 : 1;
+        }
+        answer++;
+    }
+
     return answer;
 }
