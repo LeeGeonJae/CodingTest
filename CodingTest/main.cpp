8233fc59c471b1eae6844ce1a25709762a11c6e8
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-12 01:01:58 +0900

    [코딩테스트] 프로그래머스 lv3 : 최고의 집합

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,42 +1,18 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-int solution(int x, int y, int n)
+vector<int> solution(int n, int s)
 {
-    int answer = -1;
-    queue<pair<int, int>> q;
-    vector<bool> checkBoard(1000001);
-    q.push(make_pair(0, x));
-    while (!q.empty())
+    if (s < n)
+        return { -1 };
+
+    vector<int> answer(n);
+    for (int i = 0; i < n; i++)
     {
-        int number = q.front().second;
-        int cnt = q.front().first;
-        if (number * 2 <= y && !checkBoard[number * 2])
-        {
-            q.push(make_pair(cnt + 1, number * 2));
-            checkBoard[number * 2] = true;
-        }
-        if (number * 3 <= y && !checkBoard[number * 3])
-        {
-            q.push(make_pair(cnt + 1, number * 3));
-            checkBoard[number * 3] = true;
-        }
-        if (number + n <= y && !checkBoard[number + n])
-        {
-            q.push(make_pair(cnt + 1, number + n));
-            checkBoard[number + n] = true;
-        }
-
-        if (q.front().second == y)
-        {
-            answer = cnt;
-            break;
-        }
-
-        q.pop();
+        answer[i] = s / (n - i);
+        s = s - (s / (n - i));
     }
 
     return answer;
