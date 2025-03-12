fa2d96afe312e25ce1d2d1ca3bf67f2dc9275187
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-13 06:42:19 +0900

    [코딩테스트] 프로그래머스 lv2 : 오픈채팅방 - 2019 카카오 코테

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,26 +1,34 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
+#include <sstream>
 
 using namespace std;
 
-string solution(vector<string> participant, vector<string> completion)
+vector<string> solution(vector<string> record)
 {
-    unordered_map<string, int> completionMap;
-    for (int i = 0; i < completion.size(); i++)
-        completionMap[completion[i]]++;
-
-    string answer;
-    for (string& s : participant)
+    vector<string> mecro = { "님이 들어왔습니다.", "님이 나갔습니다." };
+    unordered_map<string, string> userMap;
+    vector<pair<string, string>> inputWords;
+    for (string& s : record)
     {
-        if (!completionMap[s])
-        {
-            answer = s;
-            break;
-        }
-        else
-            completionMap[s]--;
+        stringstream ss(s);
+        string input, user, nickName;
+        ss >> input >> user >> nickName;
+
+        inputWords.push_back(make_pair(input, user));
+        if (input == "Enter" || input == "Change")
+            userMap[user] = nickName;
     }
+
+    vector<string> answer;
+    for (auto& it : inputWords)
+    {
+        if (it.first == "Enter")
+            answer.push_back(userMap[it.second] + mecro[0]);
+        if (it.first == "Leave")
+            answer.push_back(userMap[it.second] + mecro[1]);
+    }
+
     return answer;
 }
