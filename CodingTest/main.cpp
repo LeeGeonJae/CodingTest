59ecf54168b9345c684de2d9d9f63e48f61a7842
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-14 06:10:58 +0900

    [코딩테스트] 프로그래머스 lv2 : 2 x n 타일링 - 피보나치 수열

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,45 +1,13 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <unordered_map>
 
 using namespace std;
 
-pair<string, int> extract(string& s)
+int solution(int n)
 {
-    string head;
-    int number;
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (!isdigit(s[i]))
-            head += tolower(s[i]);
-        else
-        {
-            number = stoi(s.substr(i, 5));
-            break;
-        }
-    }
+    vector<int> vec(n + 1, 1);
+    for (int i = 2; i < vec.size(); i++)
+        vec[i] = (vec[i - 2] + vec[i - 1]) % 1000000007;
 
-    return make_pair(head, number);
-}
-
-vector<string> solution(vector<string> files)
-{
-    unordered_map<string, int> indexMap;
-    for (int i = 0; i < files.size(); i++)
-        indexMap[files[i]] = i;
-
-    sort(files.begin(), files.end(), [&](string& lhs, string& rhs)
-        {
-            auto lhsSort = extract(lhs);
-            auto rhsSort = extract(rhs);
-
-            if (lhsSort.first != rhsSort.first)
-                return lhsSort.first < rhsSort.first;
-            if (lhsSort.second != rhsSort.second)
-                return lhsSort.second < rhsSort.second;
-            return indexMap[lhs] < indexMap[rhs];
-        });
-
-    return files;
+    return vec[n];
 }
