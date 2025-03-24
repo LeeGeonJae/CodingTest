26534f7a994621b511582b509b1954f1dd491840
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-03-25 03:03:38 +0900

    [코딩테스트] 프로그래머스 lv2 : 두 큐 합 같게 만들기 - queue - https://school.programmers.co.kr/learn/courses/30/lessons/118667

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,54 +1,51 @@
 #include <string>
 #include <vector>
-#include <math.h>
-#include <set>
+#include <queue>
 
 using namespace std;
 
-const int NN = 10000000;
-
-void checkFuntion(set<int>& prime, vector<int>& basket, vector<bool>& checkBoard, const string& numbers, vector<bool>& primeNumber)
+int solution(vector<int> queue1, vector<int> queue2)
 {
-    if (!basket.empty())
+    queue<int> q1;
+    queue<int> q2;
+    long long lhs = 0;
+    long long rhs = 0;
+    for (int& n : queue1)
     {
-        int number = 0;
-        for (int i = 0; i < basket.size(); i++)
-            number += basket[i] * (pow(10, basket.size() - i - 1));
-
-        if (!primeNumber[number])
-            prime.insert(number);
+        q1.push(n);
+        lhs += n;
+    }
+    for (int& n : queue2)
+    {
+        q2.push(n);
+        rhs += n;
     }
 
-    for (int i = 0; i < numbers.size(); i++)
+    if ((lhs + rhs) % 2)
+        return -1;
+
+    int answer = 0;
+    while (lhs != rhs)
     {
-        if (checkBoard[i])
-            continue;
+        if (lhs < rhs)
+        {
+            q1.push(q2.front());
+            lhs += q2.front();
+            rhs -= q2.front();
+            q2.pop();
+        }
+        else if (rhs < lhs)
+        {
+            q2.push(q1.front());
+            rhs += q1.front();
+            lhs -= q1.front();
+            q1.pop();
+        }
+        answer++;
 
-        basket.push_back(numbers[i] - '0');
-        checkBoard[i] = true;
-        checkFuntion(prime, basket, checkBoard, numbers, primeNumber);
-        basket.pop_back();
-        checkBoard[i] = false;
-    }
-}
-
-int solution(string numbers)
-{
-    vector<bool> primeNumber(NN + 1);
-    primeNumber[0] = true; primeNumber[1] = true;
-    for (int i = 2; i < sqrt(NN); i++)
-    {
-        if (primeNumber[i])
-            continue;
-
-        for (int j = i * 2; j <= NN; j += i)
-            primeNumber[j] = true;
+        if (answer > (queue1.size() + queue2.size()) * 2)
+            return -1;
     }
 
-    set<int> prime;
-    vector<int> basket;
-    vector<bool> checkBoard(numbers.size());
-    checkFuntion(prime, basket, checkBoard, numbers, primeNumber);
-
-    return prime.empty() ? 0 : prime.size();
+    return answer;
 }
