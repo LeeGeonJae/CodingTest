f318599e83ee09220ffa59f23282d32db7919fd1
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-04-02 08:22:23 +0900

    [코딩테스트] 프로그래머스 lv2 : 전력망을 둘로 나누기 - DFS

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,31 +1,48 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <unordered_map>
+#include <stack>
 
 using namespace std;
 
-int solution(vector<int> players, int m, int k)
+void DFS(unordered_map<int, vector<int>>& conectedWire, int cut, int position, vector<bool>& checkBoard, stack<int>& st, int& number)
 {
-    int answer = 0;
-    queue<pair<int, int>> server;
-    int currentTime = 0;
-    int serverCnt = 0;
-    for (int& n : players)
+    vector<int>& vec = conectedWire[position];
+    for (auto& n : vec)
     {
-        currentTime++;
-        if (!server.empty() && currentTime - server.front().first >= k)
-        {
-            serverCnt -= server.front().second;
-            server.pop();
-        }
+        if (checkBoard[n] || cut == n)
+            continue;
 
-        int serverAdd = n / m - serverCnt;
-        if (serverAdd > 0)
-        {
-            server.push(make_pair(currentTime, serverAdd));
-            serverCnt += serverAdd;
-            answer += serverAdd;
-        }
+        number++;
+        checkBoard[n] = true;
+        st.push(n);
+        DFS(conectedWire, cut, st.top(), checkBoard, st, number);
+        st.pop();
+    }
+}
+
+int solution(int n, vector<vector<int>> wires)
+{
+    unordered_map<int, vector<int>> conectedWire;
+    for (int i = 0; i < wires.size(); i++)
+    {
+        conectedWire[wires[i][0]].push_back(wires[i][1]);
+        conectedWire[wires[i][1]].push_back(wires[i][0]);
+    }
+
+    int answer = 100;
+    stack<int> st;
+    for (int i = 0; i < wires.size(); i++)
+    {
+        int lhs = 0;
+        int rhs = 0;
+        vector<bool> checkBoard(n + 1);
+        checkBoard[wires[i][0]] = true;
+        checkBoard[wires[i][1]] = true;
+        DFS(conectedWire, wires[i][1], wires[i][0], checkBoard, st, lhs);
+        DFS(conectedWire, wires[i][0], wires[i][1], checkBoard, st, rhs);
+        int gap = abs(lhs - rhs);
+        answer = answer > gap ? gap : answer;
     }
 
     return answer;
