e5feb422334777a588a8470d9e6d3f249427cf72
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-04-03 19:10:12 +0900

    [코딩테스트] 프로그래머스 lv2 : 배달 - DFS - https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=cpp

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,18 +1,46 @@
-#include <string>
+#include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <set>
 
 using namespace std;
 
-string solution(int n)
+void DFS(unordered_map<int, vector<pair<int, int>>>& roadBoard, vector<bool>& visited, vector<pair<int, int>>& delivery, set<int>& answer, int K)
 {
-    string answer = "";
-    while (n)
+    int currentTown = delivery.back().first;
+    int currentLength = delivery.back().second;
+    for (auto& road : roadBoard[currentTown])
     {
-        int number = n % 3;
-        answer = (number ? to_string(number) : to_string(4)) + answer;
-        n /= 3;
-        n -= number ? 0 : 1;
+        int nextTown = road.first;
+        int nextLength = road.second;
+        if (visited[nextTown] || currentLength + nextLength > K)
+            continue;
+
+        answer.insert(nextTown);
+        visited[nextTown] = true;
+        delivery.push_back(make_pair(nextTown, currentLength + nextLength));
+        DFS(roadBoard, visited, delivery, answer, K);
+        delivery.pop_back();
+        visited[nextTown] = false;
+    }
+}
+
+int solution(int N, vector<vector<int> > road, int K)
+{
+    unordered_map<int, vector<pair<int, int>>> roadBoard;
+    for (vector<int>& vec : road)
+    {
+        roadBoard[vec[0]].push_back(make_pair(vec[1], vec[2]));
+        roadBoard[vec[1]].push_back(make_pair(vec[0], vec[2]));
     }
 
-    return answer;
+    set<int> answer;
+    vector<bool> checkBoard(N + 1);
+    vector<pair<int, int>> delivery;
+    delivery.push_back(make_pair(1, 0));
+    checkBoard[1] = true;
+    answer.insert(1);
+    DFS(roadBoard, checkBoard, delivery, answer, K);
+
+    return answer.size();
 }
