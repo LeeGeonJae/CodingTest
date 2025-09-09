526a8775f36a523a86e31181bf13b986fcf7bf94
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-09-09 21:13:23 +0900

    DFS 공부

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,34 +1,25 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
-#include <algorithm>
 
 using namespace std;
 
-class Test
+void DFS(vector<int>& numbers, int& target, int sum, int num, int& answer)
 {
-public:
-    Test();
-    ~Test();
-};
-
-vector<int> solution(vector<string> genres, vector<int> plays)
-{
-    vector<int> answer;
-    unordered_map<string, int> genresPlay;
-    unordered_map<string, vector<int>> includedSong;
-    for (int i = 0; i < genres.size(); i++)
+    if (num >= numbers.size())
     {
-        genresPlay[genres[i]] += plays[i];
+        if (target == sum)
+            answer++;
 
-        auto& song = includedSong[genres[i]];
-        if (song.size() < 2)
-            song.push_back(i);
-        else
-        {
-            int n = plays[song[0]] < plays[song[1]] ? 0 : 1;
-            if (plays[song[n]] < plays[i])
-                song[n] = i;
-        }
+        return;
     }
+
+    DFS(numbers, target, sum + numbers[num], num + 1, answer);
+    DFS(numbers, target, sum - numbers[num], num + 1, answer);
+}
+
+int solution(vector<int> numbers, int target)
+{
+    int answer = 0;
+    DFS(numbers, target, 0, 0, answer);
+    return answer;
 }
