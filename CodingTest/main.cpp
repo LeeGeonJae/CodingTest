3be881d3cb25608b7249b82bc13bf90ba481ddad
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-11-05 22:46:12 +0900

    벡터 외적 간단 풀이

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -7,30 +7,27 @@
 
 using namespace std;
 
-double solution(vector<double> A, vector<double> B)
+vector<double> solution(vector<double> A, vector<double> B)
 {
-	double dot = 0;
-	double Alength = 0;
-	double Blength = 0;
+	vector<double> cross(3, 0);
 
-	for (int i = 0; i < A.size(); i++)
-	{
-		dot += A[i] * B[i];
-		Alength += A[i] * A[i];
-		Blength += B[i] * B[i];
-	}
+	// A[1] A[2] A[0] A[1]
+	// B[1] B[2] B[0] B[1]
 
-	Alength = sqrt(Alength);
-	Blength = sqrt(Blength);
+	cross[0] = A[1] * B[2] - A[2] * B[1];
+	cross[1] = A[2] * B[0] - A[0] * B[2];
+	cross[2] = A[0] * B[1] - A[1] * B[0];
 
-	double value = dot / (Alength * Blength);
-	return acos(value) / 3.141592 * 180;
+	return cross;
 }
 
 
 int main()
 {
-	cout << solution({1, 0, 0}, {1, 2, 2});
+	vector<double> value = solution({1, 0, 0}, {0, 1, 0});
+
+	for (int i = 0; i < value.size(); i++)
+		cout << value[i] << " ";
 
 	return 0;
 }
