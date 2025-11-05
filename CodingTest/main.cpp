3db8b03137970420aaee5d096eb00e6bc7cbeaa3
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-11-05 23:02:48 +0900

    백준[11758] : CCW
    https://www.acmicpc.net/problem/11758

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-vector<double> solution(vector<double> A, vector<double> B)
+int solution(vector<int> A, vector<int> B)
 {
-	vector<double> cross(3, 0);
+	vector<int> cross(3, 0);
 
 	// A[1] A[2] A[0] A[1]
 	// B[1] B[2] B[0] B[1]
@@ -18,16 +18,32 @@ vector<double> solution(vector<double> A, vector<double> B)
 	cross[1] = A[2] * B[0] - A[0] * B[2];
 	cross[2] = A[0] * B[1] - A[1] * B[0];
 
-	return cross;
+	return cross[2] > 0 ? 1 :cross[2] == 0 ? 0 : -1;
 }
 
 
 int main()
 {
-	vector<double> value = solution({1, 0, 0}, {0, 1, 0});
+	vector<pair<int, int>> coordinate;
+	
+	for (int i = 0; i < 3; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		coordinate.push_back({ a, b });
+	}
 
-	for (int i = 0; i < value.size(); i++)
-		cout << value[i] << " ";
+	vector<int> A;
+	vector<int> B;
 
+	A.push_back(coordinate[1].first - coordinate[0].first);
+	A.push_back(coordinate[1].second - coordinate[0].second);
+	A.push_back(0);
+	B.push_back(coordinate[2].first - coordinate[1].first);
+	B.push_back(coordinate[2].second - coordinate[1].second);
+	B.push_back(0);
+
+	cout << solution(A, B);
+	
 	return 0;
 }
